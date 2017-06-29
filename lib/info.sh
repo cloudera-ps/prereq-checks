da25a1633963d7be06c6c0cba0c9de9d625080b5
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2017-06-29 16:48:42 +0900

    Use base 1024 instead of 1000 for memory report
    The output format is also changed from:
    
    RAM:           3 GB
    
    to match the Host Details of Cloudera CSI.
    
    RAM:           3.6 GB

--- lib/info.sh
+++ lib/info.sh
@@ -27,7 +27,7 @@ function print_os() {
 function print_cpu_and_ram() {
   local cpu=`grep -m1 "^model name" /proc/cpuinfo | cut -d' ' -f3- | sed -e 's/(R)//' -e 's/Core(TM) //' -e 's/CPU //'`
   print_label "CPUs" "`nproc`x $cpu"
-  print_label "RAM" "`awk '/MemTotal:/ {printf "%d GB", $2/1000/1000}' /proc/meminfo`"
+  print_label "RAM" "`awk '/MemTotal:/ {printf "%.1f GB", $2/1024/1024}' /proc/meminfo`"
 }
 
 function print_disks() {
