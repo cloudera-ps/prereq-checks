352d9b345c49e34d291516882266e626f46ee0ff
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2017-07-14 18:23:55 +0900

    Fix MySQL server check
    MySQL server package name is either of mysql-communiti-server
    (Community Edition) or mysql-commercial-server (Enterprise Edition).

--- lib/checks.sh
+++ lib/checks.sh
@@ -188,29 +188,47 @@ function check_os() {
 }
 
 function check_database() {
-  local mysql=`echo "$RPM_QA" | egrep -v "mysql-community-(common|libs|client)-" | egrep -v "mysql-(common|libs|client)-" | grep -m1 "^mysql-"`
-  if [ "$mysql" ]; then
-    local ver=`echo $mysql | cut -d'-' -f1-4`
-  else
+  local VERSION_PATTERN='([0-9][0-9]*\.[0-9][0-9]*)\.[0-9][0-9]*'
+  local mysql_ver=''
+  local mysql_rpm=''
+  local mysql_ent=`rpm -q --queryformat='%{VERSION}' mysql-commercial-server`
+  if [[ $? -eq 0 ]]; then
+    mysql_rpm=`rpm -q mysql-commercial-server`
+    [[ $mysql_ent =~ $VERSION_PATTERN ]]
+    mysql_ver=${BASH_REMATCH[1]}
+  fi
+  local mysql_com=`rpm -q --queryformat='%{VERSION}' mysql-community-server`
+  if [[ $? -eq 0 ]]; then
+    mysql_rpm=`rpm -q mysql-community-server`
+    [[ $mysql_com =~ $VERSION_PATTERN ]]
+    mysql_ver=${BASH_REMATCH[1]}
+  fi
+  if [[ -z $mysql_ver ]]; then
     state "Database: MySQL server not installed, skipping version check" 2
     return
   fi
 
-  local major_ver=`echo $ver | cut -d'-' -f4 | cut -d'.' -f1-2`
-  local msg="Database: Supported MySQL server installed. Actual: $ver"
-  if [ "$major_ver" = "5.5" ] || [ "$major_ver" = "5.6" ]; then
-    state "$msg" 0
-  else
-    state "$msg" 1
-  fi
+  case $mysql_ver in
+    '5.1') ;&
+    '5.5') ;&
+    '5.6') ;&
+    '5.7')
+      state "Database: Supported MySQL server installed. $mysql_rpm" 0
+      ;;
+    *)
+      state "Database: Unsupported MySQL server installed. $mysql_rpm" 1
+      ;;
+  esac
 }
 
 function check_jdbc_connector() {
+  # See Installing the MySQL JDBC Driver
+  # https://www.cloudera.com/documentation/enterprise/latest/topics/cm_ig_mysql.html#cmig_topic_5_5_3
   local connector=/usr/share/java/mysql-connector-java.jar
   if [ -f $connector ]; then
-    state "Database: MySQL connector is installed" 0
+    state "Database: MySQL JDBC Driver is installed" 0
   else
-    state "Database: MySQL connector is not installed" 2
+    state "Database: MySQL JDBC Driver is not installed" 2
   fi
 }
 
