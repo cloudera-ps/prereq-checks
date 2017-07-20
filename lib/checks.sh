09f296f5d277aa5fdb064d67f6d13b912a738f9c
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2017-07-20 16:42:36 +0900

    Fix SSSD check logic
    Checks /etc/nscd.conf if and only if both of NSCD and SSSD are running.

--- lib/checks.sh
+++ lib/checks.sh
@@ -274,28 +274,39 @@ function check_network() {
     _check_service_is_not_running 'Network' 'iptables'
   fi
   _check_service_is_running 'Network' 'nscd'
-  local nscd_installed=${SERVICE_STATUS['installed']}
+  local nscd_running=${SERVICE_STATUS['running']}
   _check_service_is_running 'Network' 'sssd' 2
-  local sssd_installed=${SERVICE_STATUS['installed']}
+  local sssd_running=${SERVICE_STATUS['running']}
 
-  if [[ $nscd_installed && $sssd_installed ]]; then
+  if $nscd_running && $sssd_running; then
     # 7.8. USING NSCD WITH SSSD
     # SSSD is not designed to be used with the NSCD daemon.
     # Even though SSSD does not directly conflict with NSCD, using both services
     # can result in unexpected behavior, especially with how long entries are cached.
     # https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/7/html/System-Level_Authentication_Guide/usingnscd-sssd.html
 
-    # How-to: Deploy Apache Hadoop Clusters Like a Bos
+    # How-to: Deploy Apache Hadoop Clusters Like a Boss
     # Name Service Caching
     # If you’re running Red Hat SSSD, you’ll need to modify the nscd configuration;
     # with SSSD enabled, don’t use nscd to cache passwd, group, or netgroup information.
     # http://blog.cloudera.com/blog/2015/01/how-to-deploy-apache-hadoop-clusters-like-a-boss/
-    for cached in `awk '/^[^#]*enable-cache\s+(\w+)\s+yes/ { print $2 }' /etc/nscd.conf`; do
+    for cached in `awk '/^[^#]*enable-cache.*yes/ { print $2 }' /etc/nscd.conf`; do
       case $cached in
         'passwd') ;&
         'group') ;&
         'netgroup')
           state "Network: NSCD should not cache $cached with SSSD enabled" 1
+          ;;
+        *)
+          ;;
+      esac
+    done
+    for non_cached in `awk '/^[^#]*enable-cache.*no/ { print $2 }' /etc/nscd.conf`; do
+      case $non_cached in
+        'passwd') ;&
+        'group') ;&
+        'netgroup')
+          state "Network: NSCD shoud not cache $non_cached with SSSD enabled" 0
           ;;
         *)
           ;;
