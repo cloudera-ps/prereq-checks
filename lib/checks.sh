9098e56cb06a2a4c735c4dc2d5937b1320af9323
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2017-07-21 14:44:09 +0900

    Check /tmp is mounted with noexec option or not (#66)
    * Check /tmp is mounted with noexec option or not
    
    Older versions of Cloudera Manager fail in adding a new host if
    noexec option is specified for /tmp filesystem. This problem is
    solved in 5.8.4, 5.9.2, 5.10.0 and later.
    
    * Change message for /tmp noexec mount option check
    
    -    state "System: /tmp mounted with noexec option cause troubles in older version of CMs" 0
    +    state "System: /tmp mounted with noexec fails for CM versions older than 5.8.4, 5.9.2, and 5.10.0" 0

--- lib/checks.sh
+++ lib/checks.sh
@@ -185,6 +185,18 @@ function check_os() {
   else
     state "System: Only 64bit packages should be installed" 0
   fi
+
+  local noexec=false
+  for option in `findmnt -lno options --target /tmp | tr ',' ' '`; do
+    if [[ $option = 'noexec' ]]; then
+      noexec=true
+    fi
+  done
+  if $noexec; then
+    state "System: /tmp mounted with noexec fails for CM versions older than 5.8.4, 5.9.2, and 5.10.0" 2
+  else
+    state "System: /tmp mounted with noexec fails for CM versions older than 5.8.4, 5.9.2, and 5.10.0" 0
+  fi
 }
 
 function check_database() {
