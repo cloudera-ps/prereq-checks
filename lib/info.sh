735bc67eded733ccc1e3507055dc2f7fbfcbcecf
Alex Moundalexis <alexm@cloudera.com>
2017-08-15 08:34:32 -0400

    parse /proc/meminfo for RAM instead of free()
    procps-3.2.8-25.el6.x86_64 and earlier don't support human-readable (-h) argument -- this package is 5+ years old, but we still could see it -- parse `/proc/meminfo` instead, which should exist on most Linux variants.

--- lib/info.sh
+++ lib/info.sh
@@ -35,12 +35,7 @@ function print_cpu_and_ram() {
   local cpu=`grep -m1 "^model name" /proc/cpuinfo | cut -d' ' -f3- | sed -e 's/(R)//' -e 's/Core(TM) //' -e 's/CPU //'`
   print_label "CPUs" "`nproc`x $cpu"
   # Total installed memory (MemTotal and SwapTotal in /proc/meminfo)
-  # special case for CentOS 6.5 and older which doesn't support human-readable -h flag
-  if [[ $(free -V) =~ "3.2.8" ]] ; then
-    print_label "RAM" "`free -g | awk '/Mem:/ {print $2}'`GB"
-  else
-    print_label "RAM" "`free -h | awk '/Mem:/ {print $2}'`"
-  fi
+  print_label "RAM" "$(awk '/^MemTotal:/ { printf "%.2f", $2/1024/1024 ; exit}' /proc/meminfo)G"
 }
 
 function print_disks() {
