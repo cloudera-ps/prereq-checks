8a1ffb2fdb238412f62837de0b12cbd371a889e3
sihua <sihua.an@cloudera.com>
2018-05-18 10:54:56 +0800

    Fix "{ is literal. Check expression (missing ;/\n?) or quote it."

--- prereq-check.sh
+++ prereq-check.sh
@@ -1087,7 +1087,7 @@ function check_firewall_ports(){
     local localip
     local serviceports
     local portstatus
-    localip=$(ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n')
+    localip=$(ip -4 route get 8.8.8.8 | awk "{"'print $7'"}" | tr -d '\n')
     serviceports=( 88 389 636 3268 3269 )
 
     for port in "${serviceports[@]}"; do
