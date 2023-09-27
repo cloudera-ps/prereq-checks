744dbcc99e2e071dcace67cfea865c4fa5de5be9
root <root@ccycloud.mkrhel8.root.hwx.site>
2023-09-26 23:55:02 -0700

    Changed 32bit packages check from FAIL to WARN

--- prereq-check.sh
+++ prereq-check.sh
@@ -1103,7 +1103,7 @@ function check_os() (
         local packages_32bit
         packages_32bit=$(rpm -qa --queryformat '\t%{NAME} %{ARCH}\n' | grep 'i[6543]86' | cut -d' ' -f1)
         if [ "$packages_32bit" ]; then
-            state "System: Found the following 32bit packages installed:\n$packages_32bit" 1
+            state "System: Found the following 32bit packages installed:\n$packages_32bit" 2
         else
             state "System: Only 64bit packages should be installed" 0
         fi
